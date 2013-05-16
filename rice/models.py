e477677e7fa528c77d693d304165ee7b5aaceaf6
Kunat Pipatanakul <kunato36@gmail.com>
2013-05-16 13:29:31 +0700

    final_build

--- rice/models.py
+++ rice/models.py
@@ -2,15 +2,15 @@ from django.db import models
 from django.utils.encoding import force_unicode
 # Create your models here.
 class Rice(models.Model):
-	name = models.CharField(max_length=100)
-	kind = models.CharField(max_length=100)
-	number = models.CharField(max_length=100)
-	area = models.CharField(max_length=100)
-	terrain = models.CharField(max_length=100)
-	character = models.CharField(max_length=100)
-	seed = models.CharField(max_length=100)
-	detail = models.CharField(max_length=100)
-	pic = models.CharField(max_length=100)
-	ref = models.CharField(max_length=100)
+	name = models.CharField(max_length=1000)
+	kind = models.CharField(max_length=1000)
+	number = models.CharField(max_length=1000)
+	area = models.CharField(max_length=1000)
+	terrain = models.CharField(max_length=1000)
+	character = models.CharField(max_length=1000)
+	seed = models.CharField(max_length=1000)
+	detail = models.CharField(max_length=1000)
+	pic = models.CharField(max_length=1000)
+	ref = models.CharField(max_length=1000)
 	def __unicode__(self):
 		return force_unicode(self.name)
